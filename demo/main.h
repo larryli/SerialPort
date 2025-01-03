b0322a16582aacdf13a9575b84491c1b0c14b228
Larry Li <larryli@qq.com>
2025-01-03 19:25:34 +0800

    demo

---
+++ demo/main.h
@@ -0,0 +1,9 @@
+#define DLG_MAIN  1001
+#define IDR_ICO_MAIN  8001
+
+#define ID_SERIAL 4000
+#define ID_PORTLIST 4001
+#define ID_CONNECT 4002
+#define ID_OUT 4003
+#define ID_IN 4004
+#define ID_SEND 4005
